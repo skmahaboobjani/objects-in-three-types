95de59ba99cca088e02dad4fe1eb9bf3aa251821
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2021-11-18 00:22:53 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function student(rno,name,branch,)
+{
+    this.rno=rno;
+    this.name=name;
+    this.branch=branch;
+}
+var stu1=new student("10","mabu","cse")
+console.log(stu1.rno,stu1.name,stu1.branch)
+
---
+++ script.js
@@ -0,0 +1,5 @@
+var myCar = new Object();
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+myCar.year = 1969;
+ console.log(myCar.make+" "+myCar.model )
